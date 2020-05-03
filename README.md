5253ba682883af58dcabbfc3c7b7db04f1eaeba7
Suphawich <suphawich.skv@gmail.com>
2020-05-03 20:45:04 +0700

    Update readme

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-scg-api
+# api
+
+> A Express.js project
+
+## API Setup
+
+Default endpoint: http://localhost:4000. You can change it in .env file on PORT variable. Then run again.
+And you must add other variable below
+
+```bash
+# optional
+PORT=8000
+
+# required (can see all my token in mail)
+LINE_CHANNEL_ACCESS_TOKEN=
+LINE_ME_USERID=
+GOOGLE_MAPS_API_KEY=
+```
+
+## Build Setup
+
+```bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+node src/app.js
+```
+
+For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
--- src/app.js
+++ src/app.js
@@ -1,14 +1,15 @@
-const express = require("express");
-const cors = require("cors");
-const bodyParser = require("body-parser");
-const router = require("./router");
+const express = require("express")
+const cors = require("cors")
+const bodyParser = require("body-parser")
+const router = require("./router")
+require("dotenv").config()
 
-const app = express();
-const port = process.env.PORT || 80;
+const app = express()
+const port = process.env.PORT || 4000
 
-app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use("/", router);
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use("/", router)
 
-app.listen(port);
+app.listen(port)
