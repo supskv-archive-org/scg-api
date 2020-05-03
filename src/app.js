c64c720f1be9f5237c1a69403438458a6cb4c03b
Suphawich <suphawich.skv@gmail.com>
2020-05-03 13:09:50 +0700

    Added cors

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const bodyParser = require("body-parser");
 const router = require("./router");
 
 const app = express();
 const port = process.env.PORT || 80;
 
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use("/", router);
